c8d65bea63ee6710a8ab64ffe196c1bab1a82c10
Adel Abdulazeem <adelnasr029@gmail.com>
2024-02-25 18:35:17 +0200

    new challenges solved

--- js/main.js
+++ js/main.js
@@ -300,3 +300,7 @@ let numcharOutRange = s.split('').filter(item => !rangeLetters.includes(item)).l
 return `${numcharOutRange}/${sLength}`
 }
 
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.If the function is passed a valid PIN string, return true, else return false.
+function validatePIN(pin) {
+  return /^(\d{4}|\d{6})$/.test(pin)
+}
