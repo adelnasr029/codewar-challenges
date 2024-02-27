7ceb9d0ddcd98c0623879105478f464ca49d73bd
Adel Abdulazeem <adelnasr029@gmail>
2024-02-27 17:19:59 +0200

    new challenge solved

--- js/main.js
+++ js/main.js
@@ -354,3 +354,9 @@ function solution(str, ending){
     function makeUpperCase(str) {
       return str.toUpperCase()
     }
+//Create a function which translates a given DNA string into RN
+    function DNAtoRNA(dna) {
+      return dna.split('').map(item => item === 'T'? 'U' : item)
+    }
+
+    console.log(DNAtoRNA('GCAT'))
