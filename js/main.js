2d8d606302b528e3e5156ebd7083a1d156b370b8
Adel Abdulazeem <adelnasr029@gmail.com>
2024-02-29 07:41:06 +0200

    new challenge solved

--- js/main.js
+++ js/main.js
@@ -399,3 +399,8 @@ function solution(str, ending){
     let smallest = numbers.indexOf(Math.min(...numbers)) 
     return numbers.filter((item,indx,arr) => indx !== smallest)
   }
+
+  //Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.Write a function which takes a list of strings and returns each line prepended by the correct number.The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+  var number=function(array){
+    return array.map((item,indx) => `${indx+1}: ${item}`)
+  }
