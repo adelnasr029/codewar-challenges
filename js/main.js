112f529011f3674b64efa47d0ed7f1aacd0dacfc
Adel Abdulazeem <adelnasr029@gmail.com>
2024-02-29 07:25:12 +0200

    new challenge solved

--- js/main.js
+++ js/main.js
@@ -378,7 +378,7 @@ function solution(str, ending){
   function greet (name, owner) {
     return name == owner? 'Hello boss' : 'Hello guest'
   }
-
+//Complete the function/method so that it returns the url with anything after the anchor (#) removed.
   function removeUrlAnchor(url){
     let arr = url.split('')
     if(arr.includes('#')){
@@ -393,4 +393,9 @@ function solution(str, ending){
 
   }
 
-console.log(removeUrlAnchor('www.codewars.com/katas/'))
+
+//Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list.
+  function removeSmallest(numbers) {
+    let smallest = numbers.indexOf(Math.min(...numbers)) 
+    return numbers.filter((item,indx,arr) => indx !== smallest)
+  }
