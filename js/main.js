2f811ab80c7f5bd44ade7533a942ff352b83cce6
Adel Abdulazeem <adelnasr029@gmail.com>
2024-03-01 12:55:18 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -411,3 +411,7 @@ function solution(str, ending){
     let max = Math.max(...arr)
     return [min,max]
   }
+
+//You are given an odd-length array of integers, in which all of them are the same, except for one single number.Complete the method which accepts such an array, and returns that single different number.
+  function stray(numbers) {
+   return numbers.find(num => numbers.indexOf(num) === numbers.lastIndexOf(num))  }
