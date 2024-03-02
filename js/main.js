bdfd8fc041ffc9cc8adb02161f823ecbb8a38332
Adel Abdulazeem <adelnasr029@gmail.com>
2024-03-02 20:48:41 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -445,3 +445,9 @@ function duplicateEncode(word){
  return  word.toLowerCase().split('').map((item,index,arr) => arr.indexOf(item) == arr.lastIndexOf(item)? '(' : ')').join('')
 }
 
+
+
+//Write function bmi that calculates body mass index (bmi = weight / height2).if bmi <= 18.5 return "Underweight"if bmi <= 25.0 return "Normal"if bmi <= 30.0 return "Overweight"ifbmi> 30 return "Obese"
+function bmi(weight, height) {
+  return weight/Math.pow(height,2) <= 18.5? "Underweight" : weight/Math.pow(height,2) <= 25.0? "Normal" : weight/Math.pow(height,2) <= 30.0? "Overweight" : 'Obese'
+}
