2b63a0e372ba57085d2310a1d26df71b3d926b2f
Adel Abdulazeem <adelnasr029@gmail.com>
2024-03-03 02:21:55 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -464,3 +464,9 @@ const reverseSeq = n => {
 const quarterOf = (month) => {
   return month <= 3 ? 1 : month <= 6 ? 2 : month <= 9 ? 3 : 4  
   }
+
+  //Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+  function correct(string){
+    return string.split('').map(item => item == '5'? 'S' : item == '0'? 'O' : item == '1'? 'I' : item)
+  }
+  console.log(correct("DUBL1N"))
