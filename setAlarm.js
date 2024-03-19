23f003296b1c3e86a4762563ab519ec45b559086
Adel Abdulazeem <adelnasr029@gmail.com>
2024-03-19 18:08:13 +0200

    new challenge

---
+++ setAlarm.js
@@ -0,0 +1,12 @@
+//Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+//P: 2 param of str
+//R: boolean
+//E: 
+//P:
+
+function setAlarm(employed, vacation){
+    //conditional 
+
+    return employed == true && vacation == false ? true : false
+  }
