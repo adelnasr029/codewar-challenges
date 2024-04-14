1d5298bc14d90ca6dce5d16c45ee18a974ea670c
Adel Abdulazeem <adelnasr029@gmail.com>
2024-04-14 18:13:48 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -338,15 +338,20 @@ return `${numcharOutRange}/${sLength}`
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⣏⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠤⠴⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣇⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-//P: arr of num 
-//R: num that respresent how many occurences of the most repeated num
-//E: 
+
+//P: arr of num from 0 to 9 is inclusive, intg el
+//R: the missing num that's not found within the given arr
+//E: [0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
 //P:
 
-function stantonMeasure(array){  
-  const count = array.filter(num=>num === 1).length;
-  const stanton = array.filter(num=>num === count).length;
 
-  return stanton;
+function getMissingElement(superImportantArray){
+  //TODO
+  //delcare a var of arr  nums 0 to 9
+  //loop through arr using filter method
+//return the num that the given arr missing comparing to the nums arr
+  let nums = [0,1,2,3,4,5,6,7,8,9]
+  return nums.filter(item => !superImportantArray.includes(item))[0]
 }
-console.log(stantonMeasure([1, 4, 3, 2, 1, 2, 3, 2]))
+
+console.log(getMissingElement([0, 5, 1, 3, 2, 9, 7, 6, 4]))
