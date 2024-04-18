d25251b108237bf415e20e0a6cb9e9ee9cd0446b
Adel Abdulazeem <adelnasr029@gmail.com>
2024-04-18 02:44:17 +0200

    new challenge

---
+++ questionsArr.js
@@ -0,0 +1,9 @@
+
+
+
+
+
+
+//var that hold a val of arr that contains objects
+//task is to loop through the arr and adding a usersAnswer property to each obj withing the arr
+questions.forEach(item => item.usersAnswer = null)
