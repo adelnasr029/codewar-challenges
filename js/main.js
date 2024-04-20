e0dc5da9afccf6e8716ca87fd37e695bcf13188f
Adel Abdulazeem <adelnasr029@gmail.com>
2024-04-20 12:54:15 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -340,8 +340,17 @@ return `${numcharOutRange}/${sLength}`
 
 
 
-
-
-
-
-
+//P: 2 params in form of str lamps & drone
+//R: str of 'xxxo'
+//E: 
+//P: 
+function flyBy(lamps, drone){
+//declare 2 var and split each given arg to return an arr
+//loop through the resulted arr of lamps 
+//conditional to reassign the el of the lampsArr
+//join the el of resulted arr from looping 
+  let lampsArr = lamps.split('')
+  let droneArr = drone.split('')
+return lampsArr.map((item,index) => droneArr[index] == '=' ||droneArr[index] == 'T' ? 'o' : item).join('')
+}
+console.log(flyBy('xxxxxx', '====T'))
