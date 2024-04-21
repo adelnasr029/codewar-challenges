95a4f189861d0e7806b9b3424a85cf3a00f9c8ea
Adel Abdulazeem <adelnasr029@gmail.com>
2024-04-21 23:46:16 +0200

    new challenge

---
+++ lexicograph.js
@@ -0,0 +1,11 @@
+// input: names - unsorted array
+// output: sorted array
+
+//P: arr of words
+//R: arr of words with their el arranged alphabetically
+//E: 
+//P: 
+sortme = function( names ){
+    //apply sort arr to the given names arg
+    return names.sort()
+  }
