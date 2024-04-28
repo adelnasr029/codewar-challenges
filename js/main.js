b377c3677a67f2c7a663f8cff85247a6811ce2ca
Adel Abdulazeem <adelnasr029@gmail.com>
2024-04-28 06:25:45 +0300

    new challenge

--- js/main.js
+++ js/main.js
@@ -338,30 +338,5 @@ return `${numcharOutRange}/${sLength}`
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⣏⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠤⠴⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣇⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-//P: str of letters&length > 0
-//R: another str by moving each letter by 10 position
-//E: "testcase") -->"docdmkco"
-//P:
-function moveTen(s){
-  //loop through the given str
-  //conditional based on the charCodeAt method
-  //then return the str based on the condition
- 
-  let ans = ''
-  for(let i = 0; i < s.length; ++i){
-    if(s[i].charCodeAt() + 10 > 'z'.charCodeAt()){
-      ans += String.fromCharCode(s[i].charCodeAt() - 16)
-    }else{
-      ans += String.fromCharCode(s[i].charCodeAt() + 10)
-    }
-  }
-  return ans
-}
 
-console.log(moveTen("p"))
-
-const alpha = Array.from(Array(26)).map((e,i) => i + 65)
-const alphabet = alpha.map(x => String.fromCharCode(x))
-console.log(alpha)
-
-console.log(alphabet)
+//
---
+++ numObj.js
@@ -0,0 +1,19 @@
+function numObj(s){
+return s.map(item => {
+  return {[item]: String.fromCharCode(item)}
+})
+}
+P: arr of nums
+//R: arr of objects of num prop in form of str and letter corresponds to the numv
+//E: 
+//P:
+function numObj(s){
+  //loop using  map 
+  //return form which will be as an el of the  the returned array
+
+return s.map(item => {
+  return {[item]: String.fromCharCode(item)}
+})
+}
+
+console.log(numObj([118,117,120]))
