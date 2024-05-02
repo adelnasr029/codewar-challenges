8c67bd1c3b8eb4ae74d6e8a2205ac29efcc743c1
Adel Abdulazeem <adelnasr029@gmail.com>
2024-05-02 23:16:52 +0300

    new challenge

---
+++ unusedDigits.js
@@ -0,0 +1,11 @@
+const unusedDigits = (...arr) => {
+	var digits = arr.join();
+
+	return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		.filter(x => !digits.includes(x))
+		.join('');
+};
+
+console.log(unusedDigits([12, 34, 56, 78]))
+
+console.log([12, 34, 56, 78].join())
