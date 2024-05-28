3aebcec5499ffe0006f449f3f8e25dfad17440df
Adel Abdulazeem <adelnasr029@gmail.com>
2024-05-28 23:01:25 +0300

    new challenge

--- js/main.js
+++ js/main.js
@@ -338,4 +338,16 @@ return `${numcharOutRange}/${sLength}`
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⣏⠀⠀⣼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠤⠴⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣇⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-con
+//P: str of letters and nums
+//R: str of just letters reversed comparing to the order of the given str
+//E: str = "krishan", the output should be "nahsirk"
+//P:
+
+
+function reverseLetter(str) {  
+  let alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].join('')
+
+  return str.split('').filter(el => alphabets.includes(el)).reverse().join('')
+}
+
+console.log(reverseLetter("ultr53o?n"))
