80ad16638d7f915d52cd74064b8195037b5f14fc
Adel Abdulazeem <adelnasr029@gmail.com>
2024-06-21 11:35:03 +0300

    new challenge

---
+++ arrayMash.js
@@ -0,0 +1,16 @@
+//P: 2 arr one is an arr of letters and the other is an arr of nums both arrs are of the same length
+//R: new arr that has alternating el from the 2 given arrs
+//E: eg. [1,2,3] + ['a','b','c'] = [1, 'a', 2, 'b', 3, 'c']
+//P:
+function arrayMash (array1, array2) {
+    //declare a var of []
+    //loop to reassign the declared var by pushing el from each arr each time 
+    //return the declared var after being reassigned through looping
+    let arr = []
+    for(let i = 0; i < array1.length; i++){
+      arr.push(array1[i], array2[i])
+    }
+    return arr
+  }
+  
+  console.log(arrayMash([1,2,3],['a','b','c']))/
--- js/main.js
+++ js/main.js
@@ -363,28 +363,3 @@ let max = Math.max('abc'.length, 'c'.length);
 console.log(max)
 
 
-//Palindrome is a num that is equal to the same num in reversed order
-//P: +ve intgers 
-//R: 0 if the input is a palindrome or num of the steps needed to obtain palindrome of the given num
-//P:
-
-var palindromeChainLength = function(n) {
-  //conditional to determine if n is palindrome or not
-  //if true return 0
-  //if not we declare a var of count of 0
-  //loop and keep summing the passed num with its reversed order  and repeat the same each time reassigning the count and the given num till the condition of being a a palindrome evaluates to false by equaling the reassign num with its val but reversed 
-
-  let count = 0
-  let num = n
-  for(let i = 0; i < n ; i++){
-if(num !== Number(`${num}`.split('').reverse().join(''))){
-  num += Number(`${num}`.split('').reverse().join(''))
-   count++
- }else{
-  count
- }
-  }
-return count
-};
-
-console.log(palindromeChainLength(89))
