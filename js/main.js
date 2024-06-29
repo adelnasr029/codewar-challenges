dcb688723fa488f81a648e6e5d238300a791ced4
Adel Abdulazeeem <adelnasr029@gmail.com>
2024-06-29 10:01:59 +0300

    new challenge

--- js/main.js
+++ js/main.js
@@ -362,17 +362,20 @@ let max = Math.max('abc'.length, 'c'.length);
 
 console.log(max)
 
-
-function missingWord(nums, str) {
-  //P: arr of nums not sorted & str of words 
-  //R: str of letters that their indices matches with the nums within the given arr
-  //E: [5, 0, 3], "I love you"), "ivy"
-  //P:
-  //split the given str by ' ' then join it by ''
-  //split it again by '' then loop returning el at the index with value of nums arr sorted
-  let letters = str.split(' ').join('').toLowerCase()
-  return Math.max(...nums) < str.length?  nums.sort((a,b) => a -b).map(el => letters[el]).join('') : "No mission today"
-  
-}
-
-console.log(missingWord([12, 4, 6], "Good Morning"))
+//P: 2 arrs of nums each of which consists of 3 el the 1st arr represent Alice, 2nd respresent Bob
+//R: str based on condition
+//E: solve([47, 7, 2], [47, 7, 2]),'0, 0: that looks like a "draw"! Rock on!'
+//
+function solve(a, b) {
+  //declare 2 var of 0
+  //loop 
+  //conditional to reassign that var 
+  let bobScore = 0
+  let aliceScore = 0
+  for(let i = 0; i < a.length; i++){
+      a[i] > b[i] ? aliceScore +=1 : a[i]  < b[i] ? bobScore +=1 : 0
+    
+  }
+ return aliceScore == bobScore ?`${aliceScore}, ${bobScore}: that looks like a "draw"! Rock on!` : aliceScore > bobScore ? `${aliceScore}, ${bobScore}: Alice made "Kurt" proud!` : `${aliceScore}, ${bobScore}:  Bob made "Jeff" proud!`
+  }
+console.log(solve([47, 67, 22], [26, 47, 12]))
