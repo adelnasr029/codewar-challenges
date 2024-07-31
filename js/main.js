8c3b2b6500d4ea6fe90943ea013f441af85d4a3c
Adel Abdulazeeem <adelnasr029@gmail.com>
2024-07-31 14:39:42 +0300

    new challenge

--- js/main.js
+++ js/main.js
@@ -382,11 +382,14 @@ function gauss(arr){
 
 console.log(gauss([1,2,3]))
 
-// arr of intgers 
-// num that represent the average of the these nums
-//For example: var a = [0, 1, 2];
-//
-function avg(a){
-  //loop using reduce to sum all the nums then divide it by given arr length
-  return a.reduce((acc,c) => acc + c,0)/arr.length
-}
+
+//arr of nums 
+//return new arr without duplicate elements
+// var arr = [1,1,1,2,2,3]; return [1,2,3]
+//(9) [3, 1, 9, 4, 7, 6, 5, 2, 10]
+
+  function unique(arr) {
+  //loop using filter method applying conditional that index of el === arr.lastIndexOf(el)
+  return arr.filter((el,i) => arr.indexOf(el) === i)
+  }
+  console.log(unique([ 1, 3, 1, 9, 4, 7, 6, 5, 2, 10 ]))
