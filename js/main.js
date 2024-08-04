eed9aa9db928c0e6a3574fd8a18318869cb43c81
Adel Abdulazeeem <adelnasr029@gmail.com>
2024-08-04 15:18:04 +0300

    new challenge

--- js/main.js
+++ js/main.js
@@ -383,4 +383,13 @@ function gauss(arr){
 console.log(gauss([1,2,3]))
 
 
+//2 arr of nums
+// new arr without including the elements in the 2nd arr
+//arrayDiff([1,2,2,2,3],[2]) == [1,3]
+//
+function arrayDiff(a, b) {
+  //loop filtering a elements out from b elements applying conditional
+  return a.filter(x => !b.includes(x))
+}
 
+console.log(arrayDiff([1,2,2,2,3],[2]))
