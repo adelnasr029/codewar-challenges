c548cb5d58d6602bfda8ded91a9b6ee90e6416b0
Adel Abdulazeeem <adelnasr029@gmail.com>
2024-08-05 21:13:53 +0300

    new challenge

--- js/main.js
+++ js/main.js
@@ -382,4 +382,9 @@ function gauss(arr){
 
 console.log(gauss([1,2,3]))
 
+// 2 params arr & el
+// return based on boolean num || "Not found"
+// (find(array, 5), 2)
+// using function expression to write that function that do the job in the shortest way
 
+const find = (array, element) => array.includes(element)? array.indexOf(element) : "Not found"
