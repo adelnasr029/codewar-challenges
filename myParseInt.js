3c2a1e547f9293b118b5ddcc3c7ec2f7991f4d25
Adel Abdulazeeem <adelnasr029@gmail.com>
2024-08-13 18:54:26 +0300

    new challenge

---
+++ myParseInt.js
@@ -0,0 +1,10 @@
+function myParseInt(str) {
+    //str that conatains num & letters
+    //num of the str num containted in the given str
+    //
+    //split, fiter
+  
+    return +(str.trim() + ".");
+  }
+
+  console.log(myParseInt("5 fdf"))
