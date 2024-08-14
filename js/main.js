c65209ad760a4ce56fd1347c68b5db5a65a59fa2
Adel Abdulazeeem <adelnasr029@gmail.com>
2024-08-14 18:55:30 +0300

    new challenge

--- js/main.js
+++ js/main.js
@@ -401,13 +401,34 @@ function highestRank(arr){
 
   
 
-  function myParseInt(str) {
-    //str that conatains num & letters
-    //num of the str num containted in the given str
-    //
-    //split, fiter
-  
-    return +(str.trim() + ".");
-  }
+// str of words
+// new str of words where vowel letter is converted to a num of its corresponding index + 1 in the str of "aeiou"
+// decode("h3 th2r2") would return "hi there".
+//
 
-  console.log(myParseInt("5 fdf"))
+function encode(string) {
+  //declare a var of vowel 
+  //loop
+  //replace method that will replaced each vowel with each corresponding index in the vowel var +1
+  let vowel = 'aeiou'
+  let arr = string.split('')
+  for(let i = 0; i < arr.length; i++){
+    if( vowel.includes(arr[i])){
+      arr[i] = vowel.indexOf(arr[i]) + 1
+    }
+  }
+  return arr.join('')
+}
+console.log(encode('How are you today?'))
+
+function decode(string) {
+  let vowel = 'aeiou'
+  let arr = string.split('')
+  for(let i = 1; i < string.length; i++){
+    if(Number(arr[i])){
+      arr[i] = vowel[Number(arr[i]) -1]
+    }
+  }
+  return arr.join('')
+}
+console.log(decode('h2ll4'))
