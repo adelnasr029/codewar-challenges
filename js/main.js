24e2f23dda603d09a84edc55942e54861e8ef57c
Adel Abdulazeem <adelnasr029@gmail.com>
2024-09-01 23:19:52 +0300

    new challenge

--- js/main.js
+++ js/main.js
@@ -355,18 +355,22 @@ return `${numcharOutRange}/${sLength}`
     console.log(findOdd([1,2,2,3,3,3,4,3,3,3,2,2,1]))
 
 
-
-
-// str of worlds dash or underscore delimited words, 1st word capitalized should return captilized
-// str in the form of camelCase
-//"the-stealth-warrior" gets converted to "theStealthWarrior"
+// num, ingt
+// arr of of el each is "*"
+//3 => [ "  *  "," *** ", "*****"]
 //
-function toCamelCase(str){
-// declare a var of value of the given str plited the by '-' \\ "_"
-//then loop through the declared var then nested starting looping from 2nd el converting first letter to upporcase then join
-let arr =  str.split('').map(el => el === '-'? ' ' : el == '_' ? ' ' : el).join('').split(' ')
- return arr.slice(0,1).concat(arr.slice(1).map((el,i) => el[0].toUpperCase() + el.slice(1))).join('')
- }
 
- console.log(toCamelCase("a_PippiWas-cute")) //"TheStealthWarrior"
- console.log(toCamelCase("the-stealth-warrior" )) // "theStealthWarrior"
+function towerBuilder(nFloors) {
+  // delcare a var of []
+  // loop to reassign it 
+  let result = []
+  let el = '*'
+  for(let i = 0; i < nFloors; i++){
+    result.push(' '.repeat(nFloors - i - 1) + "*".repeat((i*2)+1) + ' '.repeat(nFloors - i - 1)) 
+  }
+  return result
+}
+
+console.log(towerBuilder(3))
+
+console.log('8'.repeat(2))
