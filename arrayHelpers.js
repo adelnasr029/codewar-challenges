3e3cc7413c9c181e730585aa45a0077439e6ed7c
Adel Abdulazeem <adelnasr029@gmail.com>
2024-09-13 16:15:55 +0300

    new challenge

---
+++ arrayHelpers.js
@@ -0,0 +1,19 @@
+// TODO
+Array.prototype.square = function(){
+    return this.map( el => el * el)
+  }
+  Array.prototype.cube = function(){
+    return this.map(el => el * el * el)
+  }
+  
+  Array.prototype.average = function(){
+    return this.length > 0 ? this.reduce((acc,c) => acc + c)/this.length : NaN
+  }
+  Array.prototype.sum = function(){
+    return this.reduce((acc,c) => acc + c, 0)
+  }
+  Array.prototype.even = function(){
+  return this.filter(el => el % 2 === 0)
+  }
+  Array.prototype.odd = function(){
+    return this.filter(el => el % 2 !== 0 )
