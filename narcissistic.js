ecdab2b1e3bdbc06398aad7347d2e0b6f786822e
Adel Abdulazeem <adelnasr029@gmail.com>
2024-09-17 08:31:23 +0300

    new challenge

---
+++ narcissistic.js
@@ -0,0 +1,16 @@
+// num, +ve
+// boolean 
+// 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+//
+
+function narcissistic(value) {
+    // declare a var
+    //stringfy
+    //loop, reduce 
+    // conditioal 
+    let power = value.toString().split('').length
+    let armStrng = value.toString().split('').reduce((acc,c) => acc + Math.pow(Number(c), power), 0)
+    return armStrng === value
+  
+  }
+  console.log(narcissistic( 153))
