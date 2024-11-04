daf560b876c73a0b3ef03077b9df9ecde6ec0568
Adel Abdulazeem <adelnasr029@gmail.com>
2024-11-04 23:58:15 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -3,7 +3,19 @@
 
 
 
+//func takes n as an intg param => function counter
+// num, intger/ +ve/-ve/ always valid
+// num 
+// 
+var createCounter = function(n) {
+  // create a closure
+  // by return a func that increment n with each call
+  // closure is func with an inner func then the inner func can access var defined in the outer func scope 
+return  () => n++
+};
 
+let count = createCounter(10)
+console.log([count(), count(), count()])
 
 // const mapReduce = (array, filterFunction) => 
 //   array.reduce((acc,current) => filterFunction(current)? [...acc, current] : acc, [])
