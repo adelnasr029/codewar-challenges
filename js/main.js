591e9d7055a350a8ec18e0d5997370c5a0647440
Adel Abdulazeem <adelnasr029@gmail.com>
2024-11-09 16:00:00 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -1,18 +1,60 @@
 //abcdefghijklmnopqrstuvwxyz
 
+//func takes arr as param and return sorted arr
+function unique(arr){
+  // declare var charMap which is an {} that reassigned to keep track the value
+  // var result which [] reassigned with 
+  // for loop 
+  // then conditional to evalute whether every value we are looping through is less than the max value 
+  // push method 
+  let charMap = {}
+  let result = [arr[0]]
+  let max = 0
+  for(let i = 1; i < arr.length; i++){
+    if(!charMap[arr[i]]){
+      result.push(arr[i])
+      charMap[arr[i]] = true
+    } else {
+      charMap[arr[i]] = true
+    }
+  }
+  return new Map(result)
+}
 
-function chunkArray(array, size) {
-let result = []
-let lastArray = result[result.length -1]
-for(char of array){
-  if(!lastArray || lastArray.length === 5){
-    result.push([char])
+
+// console.log(memoTimes10(9))
+// console.log(cache)
+const times10 = (n) => n * 10
+let memoClosure = () => {
+  let cache = {}
+return (n) => {
+  if(n in cache){
+    return cache[n]
   } else {
-    lastArray.push(char)
+    let result = n * 10
+    cache[n] = result
+    return result
   }
 }
 }
-// [[0]]
-let result = [5]
-console.log(!1 == false)
-// console.log(chunkArray([0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5))
+
+// let memo1 = memoClosure()// memo1 is retaining a reference to returned value of the parent func 
+// let memo2 = memoClosure()
+// console.log(memo1(9))
+// console.log(memo2(7))
+
+const memo = (cb) => {
+  let cache = {}
+  return (n) =>{
+    if( n in cache){
+      return cache[n]
+    } else {
+      let result = cb(n)
+       cache[n] = result
+      return result
+    }
+  }
+}
+// const memoize = memo(times10)
+// console.log(memoize(5))
+
---
+++ majorityElement.js
@@ -0,0 +1,26 @@
+// func takes arr of nums => num that is present more than n/2
+// arr of nums, always valid, 
+// num which is most repetitive
+
+var majorityElement = function(nums) {
+    // loop 
+    // declare an obj to keep track every num and how many times it found
+    let numMap = {}
+    // declare a var of maxNum to store the max val of how el is found
+    // loop
+    // conditional 
+    let maxNum = 0
+    let result = 0
+    for(let num of nums){
+       numMap[num] = numMap[num] + 1 || 1
+    }
+    for(let val in numMap){
+       if(numMap[val] > maxNum){
+           maxNum = numMap[val] 
+           result = Number(val)
+       }
+    }
+    return result
+  };
+  
+  console.log(majorityElement([3,2,3]))
