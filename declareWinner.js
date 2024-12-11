80a5a7ef3dc89d1280eaf6219e185a567904516c
Adel Abdulazeem <adelnasr029@gmail.com>
2024-12-11 14:12:30 +0200

    new challenge

--- declareWinner.js
+++ declareWinner.js
@@ -1,4 +1,4 @@
-/ func takes 3 params, fighter1 & fighter2 are obj, third == str name one of the 2 fighters 
+// / func takes 3 params, fighter1 & fighter2 are obj, third == str name one of the 2 fighters 
 // 3 params first 2 are new instance each instance with 3 props, third is str
 // return str represent the name of the winner based on comparison bt health, damage per attack
 //declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"), "Lew"
@@ -9,11 +9,17 @@ function Fighter(name, health, damagePerAttack) {
     this.toString = function() { return this.name; }
 }
 function declareWinner(fighter1, fighter2, firstAttacker) {
-    if (fighter1.name !== firstAttacker) return declareWinner(fighter2, fighter1, fighter2.name)
-        if (fighter1.health <= 0) return fighter2.name
-        if (fighter2.health <= 0) return fighter1.name
-      
-        return declareWinner(new Fighter(fighter2.name, fighter2.health - fighter1.damagePerAttack, fighter2.damagePerAttack), fighter1, fighter2.name)
+    while(fighter1.health > 0 && fighter2.health > 0){
+        fighter1.health -= fighter2.damagePerAttack
+        fighter2.health -= fighter1.damagePerAttack
+    } 
+    if(fighter1.health  <= 0 && fighter2.health <= 0){
+        return firstAttacker
+    } else if(fighter1.health <= 0){
+        return fighter2.name
+    } else {
+        return fighter1.name
+    }
 }
 
 
--- js/main.js
+++ js/main.js
@@ -1,9 +1,46 @@
 //abcdefghijklmnopqrstuvwxyz
 
-// func takes 4 params first 2 are arr of nums, second 2 are intg
-// arr of nums, not always valid, nums, intg >= 0
-// return arr, sorted ascend
+
+// func takes 2 params, arr of nums, intgers & val intger
 // 
+
+
+
+var removeElement = function(nums, val) {
+    let k = 0;
+
+    // Iterate through each element in the array
+    for (let i = 0; i < nums.length; i++) {
+        // If the current element is not equal to val
+        if (nums[i] !== val) {
+            // Move it to the current k position
+            nums[k] = nums[i];
+            // Increment k
+            k++;
+        }
+    }
+
+    // Return the count of elements not equal to val
+    return k;};
+
+
+
+
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
+console.log(removeElement([3,2,2,3], 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 var merge = function(nums1, m, nums2, n) {
     //merge nums1, nums2, sorted ascend
@@ -37,23 +74,20 @@ var merge = function(nums1, m, nums2, n) {
 //   console.log(merge([1], 1, [], 0))
 //   console.log(merge([0], 0, [1], 1))
 
-// func takes 3 params, fighter1 & fighter2 are obj, third == str name one of the 2 fighters 
-// 3 params first 2 are new instance each instance with 3 props, third is str
-// return str represent the name of the winner based on comparison bt health, damage per attack
-//declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew"), "Lew"
-function Fighter(name, health, damagePerAttack) {
-    this.name = name;
-    this.health = health;
-    this.damagePerAttack = damagePerAttack;
-    this.toString = function() { return this.name; }
-}
-function declareWinner(fighter1, fighter2, firstAttacker) {
-    if (fighter1.name !== firstAttacker) return declareWinner(fighter2, fighter1, fighter2.name)
-        if (fighter1.health <= 0) return fighter2.name
-        if (fighter2.health <= 0) return fighter1.name
-      
-        return declareWinner(new Fighter(fighter2.name, fighter2.health - fighter1.damagePerAttack, fighter2.damagePerAttack), fighter1, fighter2.name)
-}
 
 
-console.log(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
