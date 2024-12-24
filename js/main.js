293bce626882fceda327d51adbbedfb431c203b6
Adel Abdulazeem <adelnasr029@gmail.com>
2024-12-24 02:45:56 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -1,38 +1,3 @@
 //abcdefghijklmnopqrstuvwxyz0123456789
 
 
- function  fromRoman(str) {
-    let romans = {
-        "M": 1000,
-        "CM": 900,
-        "D": 500,
-        "CD": 400,
-        "C": 100,
-        "XC": 90,
-        "L": 50,
-        "XL": 40,
-        "X": 10,
-        "IX": 9,
-        "v": 5,
-        "IV": 4,
-        "I": 1, 
-      }
-      let num  = 0
-    for(let i = 0; i < str.length; i++){
-        let current = romans[str[i]];
-        let next = romans[str[i + 1]];
-
-        if(current < next){
-            num -= current
-        }else{
-            num += current
-        }
-    }
-    let arr = []
-    for(let i = 0; i < num; i++){
-        arr.push(i)
-    }
-    return arr
-}
-
-console.log(fromRoman('III'))
---
+++ toBinaryNum.js
@@ -0,0 +1,6 @@
+function toBinary(n){
+    return Number(n.toString(2))
+    }
+
+    console.log(toBinary(1))
+    console.log(toBinary(10))
