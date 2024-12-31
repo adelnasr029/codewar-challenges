2691d55572520ef89d95e170e87c2749251c601b
Adel Abdulazeem <adelnasr029@gmail.com>
2024-12-31 19:49:22 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 //abcdefghijklmnopqrstuvwxyz0123456789
 
+function bubbleSort(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length; j++){
+            let temp = arr[j]
+            if(arr[j] > arr[j + 1]){
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+    return arr
+}
 
+console.log(bubbleSort([4, 1 , 2, 3]))
